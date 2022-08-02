8c22d8fd36e0a5b6342547e50be07e5a3f7a47b3
H48ILIHSAN <h48ilihsan@gmail.com>
2022-08-02 22:23:59 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>habilihsanproject</title>
+        <link href="./style.css" type="text/css" rel="stylesheet" />
+    </head>
+    <body>
+        
+        <div class="header">
+            <div class="logo">
+                <img src="./assets/logo.png" />
+            </div>
+        </div>
+        <div class="container">
+            <img src="./assets/slides/banner.png" />
+        </div>
+    
+    </body>
+</html>
+
+<?php
+    echo 'halo'
+?>
