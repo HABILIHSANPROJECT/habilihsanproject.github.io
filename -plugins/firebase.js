8be7bc384407b7513521e7bb32cd80ea97d880ed
H48ILIHSAN <h48ilihsan@gmail.com>
2023-03-31 09:52:42 +0700

    NEBULA PROJECT

--- -plugins/firebase.js
+++ -plugins/firebase.js
@@ -2,7 +2,7 @@
     // Import the functions you need from the SDKs you need
     import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js"
     import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-analytics.js"
-    import { getDatabase, ref, child, get} from "https://www.gstatic.com/firebasejs/9.19.0/firebase-database.js"
+    import { getDatabase } from "https://www.gstatic.com/firebasejs/9.19.0/firebase-database.js"
     // TODO: Add SDKs for Firebase products that you want to use
     // https://firebase.google.com/docs/web/setup#available-libraries
   
