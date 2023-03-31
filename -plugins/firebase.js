b3a45631cf282bc63eeb60126c6303fdb8b131f6
H48ILIHSAN <h48ilihsan@gmail.com>
2023-03-31 09:57:54 +0700

    NEBULA PROJECT

--- -plugins/firebase.js
+++ -plugins/firebase.js
@@ -1,25 +1,36 @@
 
-    // Import the functions you need from the SDKs you need
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js"
-    import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-analytics.js"
-    import { getDatabase } from "https://www.gstatic.com/firebasejs/9.19.0/firebase-database.js"
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
-  
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    const firebaseConfig = {
-      apiKey: "AIzaSyDSDIWEIdJuA2Z3tVC8fIS8eFvRG8HkXwQ",
-      authDomain: "habilihsanproject13.firebaseapp.com",
-      databaseURL: "https://habilihsanproject13.firebaseio.com",
-      projectId: "habilihsanproject13",
-      storageBucket: "habilihsanproject13.appspot.com",
-      messagingSenderId: "479562057640",
-      appId: "1:479562057640:web:432df6f60898cef2297f9d",
-      measurementId: "G-LM5DS0C4KS"
-    }
-  
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig)
-    const analytics = getAnalytics(app)
-    const database = getDatabase(app)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js"
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-analytics.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.19.0/firebase-database.js"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDSDIWEIdJuA2Z3tVC8fIS8eFvRG8HkXwQ",
+  authDomain: "habilihsanproject13.firebaseapp.com",
+  databaseURL: "https://habilihsanproject13.firebaseio.com",
+  projectId: "habilihsanproject13",
+  storageBucket: "habilihsanproject13.appspot.com",
+  messagingSenderId: "479562057640",
+  appId: "1:479562057640:web:432df6f60898cef2297f9d",
+  measurementId: "G-LM5DS0C4KS"
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+const database = getDatabase(app)
+
+const dbRef = ref(database)
+get(child(dbRef, `navbar`)).then((snapshot) => {
+  if (snapshot.exists()) {
+    console.log(snapshot.val())
+  } else {
+    console.log("No data available")
+  }
+}).catch((error) => {
+  console.error(error)
+})
