59419eb324b8452c8cc59f260226dd5291950c20
H48ILIHSAN <h48ilihsan@gmail.com>
2023-03-31 23:56:25 +0700

    NEBULA PROJECT

--- -plugins/firebase.js
+++ -plugins/firebase.js
@@ -20,7 +20,6 @@ const firebaseConfig = {
 }
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
-const database = getDatabase(app)
-localStorage.setItem("data", database)
+let app = initializeApp(firebaseConfig)
+let analytics = getAnalytics(app)
+let database = getDatabase(app)
