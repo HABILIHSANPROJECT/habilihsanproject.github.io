644055336050914fe66c3cd7b9162ad49b17000c
H48ILIHSAN <h48ilihsan@gmail.com>
2023-09-09 18:48:21 +0700

    JKT48 THEATER

--- jkt48-theater/main.js
+++ jkt48-theater/main.js
@@ -1,5 +1,5 @@
 $(window).on('load', function () {
-    $('#m3u8-placeholder')[0].value = localStorage.getItem('m3u8-link') || '';
+    $('#m3u8-placeholder')[0].value = "https://hls-origin213.showroom-cdn.com/liveedge/ngrp:89f9bdbbddbac79d7c7be67799c8417c61c84bcaf80398d774aa3d6ab3070808_all/chunklist_b3399680.m3u8" || '';
     $('#play-btn').on('click', function () {
         localStorage.setItem('m3u8-link', $('#m3u8-placeholder')[0].value);
         window.location.href = './player' + '#' + $('#m3u8-placeholder')[0].value;
