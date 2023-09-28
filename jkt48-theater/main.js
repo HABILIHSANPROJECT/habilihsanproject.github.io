f3e4a4852f6e020547cca1ace97dc8c6288db99b
H48ILIHSAN <h48ilihsan@gmail.com>
2023-09-28 19:10:34 +0700

    JKT48 THEATER

--- jkt48-theater/main.js
+++ jkt48-theater/main.js
@@ -1,7 +1,7 @@
 $(window).on('load', function () {
-    $('#m3u8-placeholder')[0].value = "https://hls-origin213.showroom-cdn.com/liveedge/ngrp:89f9bdbbddbac79d7c7be67799c8417c61c84bcaf80398d774aa3d6ab3070808_all/chunklist_b3399680.m3u8" || '';
+    //$('#m3u8-placeholder')[0].value = "https://hls-origin213.showroom-cdn.com/liveedge/ngrp:89f9bdbbddbac79d7c7be67799c8417c61c84bcaf80398d774aa3d6ab3070808_all/chunklist_b3399680.m3u8" || '';
     $('#play-btn').on('click', function () {
-        localStorage.setItem('m3u8-link', $('#m3u8-placeholder')[0].value);
-        window.location.href = './player' + '#' + $('#m3u8-placeholder')[0].value;
+        //localStorage.setItem('m3u8-link', $('#m3u8-placeholder')[0].value);
+        window.location.href = './player/index.html#https://hls-origin276.showroom-cdn.com/liveedge/ngrp:4bc3f677066576f680862a4cb20b857040bbff0f89afb4150ca49c0d17838b77_all/playlist.m3u8'
     });
 });
