9ec03329bfd239509f501b5fe29cb7924df5e73c
H48ILIHSAN <h48ilihsan@gmail.com>
2023-10-06 00:09:33 +0700

    JKT48 THEATER

--- jkt48-theater/index.js
+++ jkt48-theater/index.js
@@ -1,4 +1,14 @@
 
+const schedule = []
+const header = {
+    headers : {
+        apikey: "NGIDOLKAKAK"
+    } }
+axios.get("https://idol48.top/event", header).then(function(response) {
+    schedule = response.data.events
+    console.log(schedule)
+})
+
 document.addEventListener("DOMContentLoaded", function () {
     const player = videojs("video-player", {
         controls: true,
@@ -15,4 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
         src: "https://hls-origin280.showroom-cdn.com/liveedge/7612e3ed2f3f9a3f94882b2413c82d9119f9ef7463bb9a88ab2a6660fdcf6a9b_source/chunklist.m3u8",
         type: "application/x-mpegURL"
     })
+    player.on('error', function (e) {
+        document.querySelector(".vjs-modal-dialog-content").innerText = "Tidak ada show yang berlangsung saat ini!"
+      })
 })
