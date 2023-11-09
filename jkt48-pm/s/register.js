343cee35fd598d16052d1605d32fceebf3d3ef39
H48ILIHSAN <h48ilihsan@gmail.com>
2023-11-09 14:43:49 +0700

    JKT48 PRIVATE MESSAGE

--- jkt48-pm/s/register.js
+++ jkt48-pm/s/register.js
@@ -35,7 +35,7 @@ axios.get("https://raw.githubusercontent.com/HABILIHSANPROJECT/habilihsanproject
                 .then(() => {
                     firebase.auth().onAuthStateChanged((user) => {
                         if (user) {
-                            location.replace("https://habilihsanproject.mayar.link/s/jkt48-private-message")
+                            location.replace("../p/subs.html")
                         } else {
                             location.reload()
                         }
