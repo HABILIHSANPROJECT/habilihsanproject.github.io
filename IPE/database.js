e108941ba84e5c701839a472a5dc3cab625e4df3
H48ILIHSAN <h48ilihsan@gmail.com>
2024-02-10 22:23:40 +0700

    IPE

---
+++ IPE/database.js
@@ -0,0 +1,10 @@
+const konfigurasi = {
+    apiKey: "AIzaSyCihh4EWt7zm3cBRqUd5q12qaNa0gk2Gwc",
+    authDomain: "ipe2024.firebaseapp.com",
+    projectId: "ipe2024",
+    storageBucket: "ipe2024.appspot.com",
+    messagingSenderId: "408874586451",
+    appId: "1:408874586451:web:a5dc883a3aee02298751a6"
+}
+
+firebase.initializeApp(konfigurasi)
