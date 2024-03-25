6b728c8f7c76047f765abb8d79185d627b41b917
H48ILIHSAN <h48ilihsan@gmail.com>
2024-03-25 21:47:11 +0700

    JKT48 PM

--- jkt48-pm/s/index.js
+++ jkt48-pm/s/index.js
@@ -395,30 +395,29 @@ axios.get("https://raw.githubusercontent.com/HABILIHSANPROJECT/habilihsanproject
                 location.replace("https://habilihsanproject.github.io/jkt48-pm/p/login")
             }
         })
+        if (document.getElementById("exits")) {
+            document.getElementById("exits").addEventListener("click", () => {
+                firebase.auth().signOut()
+                    .then(() => {
+                        showSuccess("Logout berhasil!")
+                        location.replace("https://habilihsanproject.github.io/jkt48-pm/p/login")
+                    })
+                    .catch((error) => {
+                        showError(error.message)
+                    })
+            })
+        }
+        
+        if (document.getElementById("exit")) {
+            document.getElementById("exit").addEventListener("click", () => {
+                firebase.auth().signOut()
+                    .then(() => {
+                        showSuccess("Logout berhasil!")
+                        location.replace("https://habilihsanproject.github.io/jkt48-pm/p/login")
+                    })
+                    .catch((error) => {
+                        showError(error.message)
+                    })
+            })
+        }
     })
-
-    if (document.getElementById("exits")) {
-        document.getElementById("exits").addEventListener("click", () => {
-            firebase.auth().signOut()
-                .then(() => {
-                    showSuccess("Logout berhasil!")
-                    location.replace("https://habilihsanproject.github.io/jkt48-pm/p/login")
-                })
-                .catch((error) => {
-                    showError(error.message)
-                })
-        })
-    }
-    
-    if (document.getElementById("exit")) {
-        document.getElementById("exit").addEventListener("click", () => {
-            firebase.auth().signOut()
-                .then(() => {
-                    showSuccess("Logout berhasil!")
-                    location.replace("https://habilihsanproject.github.io/jkt48-pm/p/login")
-                })
-                .catch((error) => {
-                    showError(error.message)
-                })
-        })
-    }
